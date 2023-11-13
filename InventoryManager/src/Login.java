c165f6627d4a01b89476828236cd91e20b129d40
Nicholas <nicholaseovva@csus.edu>
2023-11-13 01:46:15 -0800

    optimized login class by placing gbc gridx, gridy and insets into helper method. Slightly increased username and password textboxes. Added "create account" hyperlink under login button.

--- InventoryManager/src/Login.java
+++ InventoryManager/src/Login.java
@@ -1,11 +1,15 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.*;
 
@@ -14,36 +18,59 @@ public class Login{
 	public static void showLandingPage() {
 		
 		GridBagConstraints gbc = new GridBagConstraints();
+		
 		JFrame frame = new JFrame("Inventory Manager");
 		JPanel panel = new JPanel(new GridBagLayout());
+		panel.setBackground(Color.gray);
+		
 		JLabel usernameText = new JLabel("Username");
         JLabel passwordText = new JLabel("Password");
-        JTextField usernameField = new JTextField(10);
-        JPasswordField passwordField = new JPasswordField(10);
+        JTextField usernameField = new JTextField();
+        JPasswordField passwordField = new JPasswordField();
         JButton loginButton = new JButton("Login");
+        JLabel addAccount = new JLabel("Don't have an account? Sign up here!");
+     
+     
+        usernameField.setPreferredSize(new Dimension(200, 35));
+        passwordField.setPreferredSize(new Dimension(200, 35));
+        loginButton.setPreferredSize(new Dimension(200, 35));
         
-        panel.setBackground(Color.gray);
-        gbc.insets = new Insets(10, 40, 0, 0);
-        panel.add(usernameText, gbc);
-        gbc.gridx = 1;
-        gbc.gridy = 0;
-        gbc.insets = new Insets(10, 0, 0, 80);
-        panel.add(usernameField, gbc);
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        gbc.insets = new Insets(10, 40, 0, 0);
-        panel.add(passwordText, gbc);
-        gbc.gridx = 1;
-        gbc.gridy = 1;
-        gbc.insets = new Insets(10, 0, 0, 80);
-        panel.add(passwordField, gbc);
-        gbc.gridx = 1;
-        gbc.gridy = 2;
-        panel.add(loginButton, gbc);
+  
+        addAccount.setFont(new Font("Arial", Font.PLAIN, 12));
+        addAccount.setForeground(Color.orange);
+        addAccount.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        addAccount.addMouseListener(new MouseAdapter() {
+        	public void mouseClicked(MouseEvent e) {
+        		JOptionPane.showMessageDialog(frame, "This page will pop up with two text boxes to register a new username and password");
+        	}
+        });
+ 
         
+        AddToUI(panel, usernameText, 0,0,10,40,0,0);
+        AddToUI(panel, usernameField, 1,0,10,0,0,80);
+        AddToUI(panel, passwordText, 0,1,10,40,0,0); 
+        AddToUI(panel, passwordField, 1,1,10,0,0,80);  
+        AddToUI(panel, addAccount, 1,2,10,0,-160,80);
+        AddToUI(panel, loginButton, 1,3,10,0,-100,80);
+   
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel, BorderLayout.WEST);
         frame.setVisible(true);
 	}
+	
+	private static void AddToUI (JPanel panel, Component comp, int x, int y, int top, int left, int bottom, int right) {
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = x;
+		gbc.gridy = y;
+		gbc.insets = new Insets(top, left, bottom, right);
+		panel.add(comp, gbc);
+		
+		
+	}
+	
+	
+	
+	
+	
 
 }
